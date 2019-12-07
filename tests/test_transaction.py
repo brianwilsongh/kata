c2836d84c1b6aa320c6190851c067f23bab36f74
wilsontheory <wilson.brian90@gmail.com>
2019-12-07 14:37:35 -0500

    Add a quick test to cover item quantity storage and add new test for deleting from transaction

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -11,13 +11,21 @@ class TestTransaction(unittest.TestCase):
         self.inv.add("caviar", 46.52, by_weight=True)
         self.inv.add("nutella", 7.99)
         self.inv.add("corn", 1.99, by_weight=True)
-    
-    def test_add_item_to_transaction_is_stored(self):
         self.trans.add(self.inv, "corn", 3)
         self.trans.add(self.inv, "caviar", 41)
-        self.assertEqual(2, len(self.trans.items))
+        self.trans.add(self.inv, "nutella", 842)
+    
+    def test_add_item_to_transaction_is_stored(self):
+        # self.trans.add(self.inv, "corn", 3)
+        # self.trans.add(self.inv, "caviar", 41)
+        self.assertEqual(3, len(self.trans.items))
         
     def test_add_item_tracks_quantity(self):
-        self.trans.add(self.inv, "corn", 842)
-        self.assertEqual(842, self.trans.items["corn"]["quant"])
+        # self.trans.add(self.inv, "corn", 842)
+        self.assertEqual(842, self.trans.items["nutella"]["quant"])
+    
+    def test_remove_item_from_transaction(self):
+        self.trans.delete("corn")
+        self.assertIsNone(self.items.get("corn"))
+        self.assertEquals(2, len(self.items("corn")))
         
