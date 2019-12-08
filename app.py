f17cb5124c6c64989d41db978bde3230c2cbb154
wilsontheory <wilson.brian90@gmail.com>
2019-12-08 14:44:05 -0500

    Fix readme and add some code in app.py to test it out

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import argparse
 import unittest
+import inventory
+import discount
+import transaction
 
 class App:
     
@@ -9,13 +12,24 @@ class App:
     def run(self):
         print("Running Application!")
         #in here we will initiate required object, accept scanned items, display total
+        self.inv = inventory.Inventory()
+        self.inv.add("beef", 3.99, by_weight=True)
+        self.inv.add("milk", 2.99)
+        self.inv.add("carrot", 1.99, by_weight=True)
+        self.inv.add("cashew", 6.99, by_weight=True)
+        self.inv.delete("cashew")
+        self.inv.modify("beef", 4.99, by_weight=True)
+        
+        self.disc = discount.Discount()
+        self.disc.add_markdown("milk", 5)
+        
+        self.trans = transaction.Transaction()
+        self.trans.add(self.inv, "beef", 3)
+        self.trans.add(self.inv, "carrot", 21)
+        
+        print("that will be: " + str(self.trans.get_total(self.inv, self.disc)))
+        
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-V", "--verbose", help="Produce detailed output", action="store_true")
-    args = parser.parse_args()
-    if args.verbose:
-        print("Produce verbose output")
-    print("Starting app.py!")
     app = App()
     app.run()
